673cf4a3b286c2d7b7c064ca6b931755f129c1f2
Lucas Garcia Nachtigall <69644385+lucas-nachtigall@users.noreply.github.com>
2023-01-24 21:02:25 -0300

    Update index.js

--- api/core/index.js
+++ api/core/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello World!!12'),
+        body: JSON.stringify('Hello World!!132'),
     };
     return response;
 };
